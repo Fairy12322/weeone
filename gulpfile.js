d71b46da92052798640422acb1b9d7b2d09ff690
Fairy12322 <1993162075@qq.com>
2018-11-05 09:34:39 +0800

    初始化项目

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,35 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass"); //编译scss
 var auto = require("gulp-autoprefixer"); //自动添加前缀
-var clean = require("gulp-clean-css") //压缩css
-var server = require("gulp-webserver") //拦截
+var clean = require("gulp-clean-css"); //压缩css
+var server = require("gulp-webserver"); //拦截
 
-gulp.task("devScss", function() {
-    return gulp.src("./src/scss/*.scss")
-        .pipe(sass())
-        .pipe(auto({
-            browsers: ['last 2 versions'],
-        }))
-        .pipe(gulp.dest('./src/css'))
-})
+var path = require("path");
+var url = require("url");
+var fs = require("fs")
 
-gulp.task("watch", function() {
-    return gulp.src("./src/scss/*.scss", serise("devScss"))
-})
+// gulp.task("devScss", function() {
+//     return gulp.src("./src/scss/*.scss")
+//         .pipe(sass())
+//         .pipe(auto({
+//             browsers: ['last 2 versions'],
+//         }))
+//         .pipe(gulp.dest('./src/css'))
+// });
+
+// gulp.task("watch", function() {
+//     return gulp.src("./src/scss/*.scss", serise("devScss"))
+// });
+
+gulp.task("devServer", function() {
+    return gulp.src('./src')
+        .pipe(server({
+            livereload: true,
+            open: true,
+            middleware: (function(req, res, next) {
+                pathname = url.parse(req.url).pathname;
+                pathname = pathname === "index.html" ? pathname : pathname;
+                res.end(fs.readFileSync(path.join(__dirname, "src", pathname)))
+            })
+        }));
+});
